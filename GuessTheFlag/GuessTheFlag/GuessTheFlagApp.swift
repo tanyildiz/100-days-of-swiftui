c0137561a2d99a4c4a45856b7b4f64c9c48ba5d6
tanyildiz <tanyildizn@gmail.com>
2025-09-14 13:50:10 +0300

    Guess the Flag project with challenges

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Niyazi on 12.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
