490173aeb2e624613b8ed47aa1a73cd59f4bf3cb
Niyazi <tanyildizn@gmail.com>
2025-10-17 23:46:54 +0300

    First initial for Animations project

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Niyazi on 17.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
